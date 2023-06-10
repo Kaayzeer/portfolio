03ae8a481593929b34987c79f98cec5692d5cfb1
Kaayzeer <mautiagiro@gmail.com>
2023-06-10 11:01:49 +0200

    filestructure and layout created

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Head from "next/head";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+import Navbar from "./Navbar/Navbar.jsx";
+import Footer from "./Footer/Footer.jsx";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>Portfolio</title>
+        <meta name="description" content="My portfolio" />
+        <meta
+          name="keywords"
+          content="Frontend developer, portfolio of works in related field with Html5, CSS3, Javascript, React.js and Next.js latest technologies"
+        />
+      </Head>
+      <Navbar />
+      <main className={`${inter.className}`}>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
