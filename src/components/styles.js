13eb7db6692faf30f8e00c1d0183f83255532652
Kaayzeer <mautiagiro@gmail.com>
2023-06-22 08:35:23 +0200

    removes testcolors from containers

--- src/components/styles.js
+++ src/components/styles.js
@@ -1,46 +1,46 @@
 export const ContainerFullWidthStyles = {
-  minWidth: '100%',
-  bg: 'blue',
+  minWidth: "100%",
+  /*   bg: 'blue', */
 };
 
 export const ContainerMaxWidthStyles = (cols = 2) => {
   return {
-    display: 'grid',
-    placeItems: 'center',
-    gap: 'rem',
-    bg: 'green',
-    padding: '3rem 1.5rem',
-    '@xs': {
+    display: "grid",
+    placeItems: "center",
+    gap: "rem",
+    /*     bg: 'green', */
+    padding: "3rem 1.5rem",
+    "@xs": {
       gridTemplateColumns: `repeat(${cols},1fr)`,
-      gap: '2rem',
-
+      gap: "2rem",
+      /* 
       bg: 'purple',
-      bg: 'red',
-      padding: '5rem 7rem',
+      bg: 'red', */
+      padding: "5rem 7rem",
     },
-    '@lg': {
-      padding: '6rem 7rem',
+    "@lg": {
+      padding: "6rem 7rem",
     },
   };
 };
 
 export const ContainerNoPaddingMaxWidthStyles = (cols = 2) => {
   return {
-    display: 'grid',
-    placeItems: 'center',
-    gap: 'rem',
-    bg: 'green',
+    display: "grid",
+    placeItems: "center",
+    gap: "rem",
+    /*     bg: 'green', */
 
-    '@xs': {
+    "@xs": {
       gridTemplateColumns: `repeat(${cols},1fr)`,
-      gap: '2rem',
+      gap: "2rem",
 
-      bg: 'purple',
-      bg: 'red',
+      /*     bg: 'purple',
+      bg: 'red', */
     },
-    '@lg': {
-      bg: 'red',
+    "@lg": {
+      /*    bg: 'red', */
     },
-    '@xl': {},
+    "@xl": {},
   };
 };
